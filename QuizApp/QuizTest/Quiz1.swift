ede4269097bc5a8cbca2c8425275481d8f7329c5
PapiaMCode <papiamcode@gmail.com>
2022-07-13 03:27:43 +0600

    Added: Image and Question view

--- QuizApp/QuizTest/Quiz1.swift
+++ QuizApp/QuizTest/Quiz1.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
+//view for the quiz game
 struct Quiz1: View {
+    
+    //number of question
+    @State var i : Int = 0
+    
+    //var for thr score
+    @State var score = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 15) {
+            
+            //if i < of question -> play Question
+            if(self.i < myQuiz2.count){
+                
+                //image of the question
+                Image(myQuiz2[self.i].img! )
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal)
+                
+                //text of the question
+                Text(myQuiz2[self.i].text!)
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
