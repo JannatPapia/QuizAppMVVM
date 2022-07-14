2f0405b37e1d27b20ab859cb10a0e07f191f13c6
PapiaMCode <papiamcode@gmail.com>
2022-07-14 22:57:40 +0600

    Create new group for firebase quiz app

---
+++ QuizApp/FirebaseQuizApp/FirebaseQuizAppContentView.swift
@@ -0,0 +1,20 @@
+//
+//  FirebaseQuizAppContentView.swift
+//  QuizApp
+//
+//  Created by Jannatun Nahar Papia on 14/7/22.
+//
+
+import SwiftUI
+
+struct FirebaseQuizAppContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirebaseQuizAppContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirebaseQuizAppContentView()
+    }
+}
---
+++ QuizApp/FirebaseQuizApp/firebaseView/firebaseHome.swift
@@ -0,0 +1,20 @@
+//
+//  firebaseHome.swift
+//  QuizApp
+//
+//  Created by Jannatun Nahar Papia on 14/7/22.
+//
+
+import SwiftUI
+
+struct firebaseHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct firebaseHome_Previews: PreviewProvider {
+    static var previews: some View {
+        firebaseHome()
+    }
+}
